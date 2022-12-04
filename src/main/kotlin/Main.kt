0a9e8371c450902305d1591d9cd91f0d85181f08
Mike Macaulay <michael.macaulay@aviture.us.com>
2022-12-04 13:48:46 -0600

    06 done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,23 +5,45 @@ import Game.Play.*
 fun main(args: Array<String>) {
 
     val lines = (File("05input.txt").readLines())
-    val compartments = lines.map { line ->
-        val first = line.substring(0, line.length / 2);
-        val second = line.substring(line.length / 2)
+//    val lines = """
+//        vJrwpWtwJgWrhcsFMMfFFhFp
+//        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+//        PmmdzqPrVvPwwTWBwg
+//        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+//        ttgJtRGJQctTZtZT
+//        CrZsJsPPZsGzwwsLwLmpwMDw
+//    """.trimIndent().split("\n")
+    val compartments = lines.chunked(3)
+        .map { (first, second, third) ->
+            first.find { firstChar ->
+                val found = second.find { secondChar ->
+                    val thirdFound = third.find { thirdChar ->
+                        firstChar == secondChar && secondChar == thirdChar
+                    }
+                    thirdFound != null
+                }
+                found != null
+            }
+        }
+    println(compartments)
+
+//    map { line ->
+//        val first = line.substring(0, line.length / 2);
+//        val second = line.substring(line.length / 2)
 //        println(line)
 //        println("$first : $second")
-        listOf(first, second)
-    }
-
-    val found = compartments.map{ (first, second) ->
-       println("ummm $first : $second")
-       first.find { firstChar ->
-            val found = second.find { secondChar ->
-                firstChar == secondChar
-            }
-            (found != null)
-        }
-    }
+//        listOf(first, second)
+//    }
+//
+//    val found = compartments.map{ (first, second) ->
+//       println("ummm $first : $second")
+//       first.find { firstChar ->
+//            val found = second.find { secondChar ->
+//                firstChar == secondChar
+//            }
+//            (found != null)
+//        }
+//    }
 
     val priorityMap = HashMap<Char,Int>()
     var value = 1;
@@ -32,7 +54,8 @@ fun main(args: Array<String>) {
         priorityMap[i] = value++;
     }
     println(priorityMap)
-    val ranking = found.map {
+
+    val ranking = compartments.map {
         priorityMap[it]!!
     }.sum()
     println("results = $ranking")
