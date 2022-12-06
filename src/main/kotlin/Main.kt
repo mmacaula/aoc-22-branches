8a504588fef1a48b28fc78185103f6adbf5d7bbd
Mike Macaulay <michael.macaulay@aviture.us.com>
2022-12-06 10:21:25 -0600

    day6-2 and 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,70 +1,37 @@
-import java.io.File
-
 import Game.Play.*
-import kotlin.math.log
+import java.io.File
 
 fun main(args: Array<String>) {
 
-    val lines = (File("05input.txt").readLines())
+    val lines = (File("06input.txt").readLines())
 //    val lines = """
-//    [D]
-//[N] [C]
-//[Z] [M] [P]
-// 1   2   3
-//
-//move 1 from 2 to 1
-//move 3 from 1 to 3
-//move 2 from 2 to 1
-//move 1 from 1 to 2
+//zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
 // """.trimIndent().split("\n")
 
+    val line = lines[0];
+    val initial = line.substring(0,14);
+    val rest = line.substring(14)
+    var initialList = initial.toList()
+    var deque = ArrayDeque<Char>(initialList)
+    println(deque);
+    rest.forEachIndexed{ i, char ->
 
-    val index = lines.indexOf("");
-    val stacksLines = lines.subList(0,index - 1)
-    val columnsLine = lines.subList(index-1, index)[0]
-    val instructionsLines  = lines.subList(index+1, lines.lastIndex+1)
-
-    val columns = columnsLine.split(" ").filter { it.isNotEmpty() }
-
-    val stacks = columns.map {
-        ArrayDeque<String>()
-    }
-    val rows = stacksLines.map{
-        val regex = """....?""".toRegex()
-        regex.findAll(it).map {
-            it.value.trim()
-        }.toList()
-
-    }
-    rows.forEach { row ->
-        row.forEachIndexed { i, item ->
-            if(!item.isEmpty()){
-                stacks[i].add(item.substring(1,2));
-            }
+        deque.removeFirst();
+        deque.addLast(char)
+        println("after processing $char, deque is $deque")
+        if(deque.toSet().size == 14){
+            println("Deque set: ${deque.toSet()}")
+            println (i + 15 )
+            return
         }
     }
 
-    println("Stacks: ${stacks}")
-    val instructions = instructionsLines.map{
-        val words = it.split(" ")
-        listOf(words[1].toInt(), words[3].toInt()-1, words[5].toInt()-1)
-    }.forEach{ instruction ->
-        val (times, from, to ) = instruction;
-
-        val items = stacks[from].subList(0,times).toList()
-        println("items: $items")
-        repeat(times){ stacks[from].removeFirst()}
-        println("from: ${stacks[from]}")
-        println("items: $items")
-        stacks[to].addAll(0, items)
-        println("items: $items")
-        println("after move instruction ${instruction}: $stacks")
-    }
-    val result = stacks.map {
-        it.removeFirst()
-    }.joinToString("") { it }
+    println(initialList)
+//    val results = line.fold(ArrayDeque<String>()) { acc, c ->
+//        if(acc.len)
+//    }
 
 
-    println("columns: ${result}")
+    println("columns: ${rest}")
 
 }
